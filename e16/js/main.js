abe9218ae8b98acd35debffc26f537e8ecb74bf0
Louisa <ramirezlm12@gmail.com>
2019-04-04 10:39:57 -0400

    hw

---
+++ e16/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ // $('h1').css('color','white');
+ $('#number5').click(function() {
+   $('#number5').css('color','white');
+   $('#bird-5').toggleClass('pink');
+ });
+ $('#number4').click(function() {
+    $('#number4').css('color', 'goldenrod');
+    $('#bird-4').toggleClass('green');
+  });
+
+});
